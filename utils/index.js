999a64f7d6414d29a3ff0010fb0c04905123ad7f
Scott Rinaberger <scottrinaberger@gmail.com>
2022-10-05 15:32:47 -0500

    added searches and queries

---
+++ utils/index.js
@@ -0,0 +1,152 @@
+class DB {
+	constructor(connection) {
+		this.connection = connection;
+	}
+
+	// Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+	findAllEmployees() {
+		return this.connection
+			.promise()
+			.query(
+				"SELECT employee.id id, employee.first_name, employee.last_name, roles.title, department.name, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) manager FROM employee LEFT JOIN roles on employee.role_id = roles.id LEFT JOIN department on roles.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+			);
+	}
+
+	// Create a new employee
+	createEmployee(employee) {
+		return this.connection
+			.promise()
+			.query('INSERT INTO employee SET ?', employee);
+	}
+
+	// Remove an employee with the given id
+	removeEmployee(employeeId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM employee WHERE id = ?', employeeId);
+	}
+
+	// Update the given employee's role
+	updateEmployeeRole(employeeId, roleId) {
+		return this.connection
+			.promise()
+			.query('UPDATE employee SET role_id = ? WHERE id = ?', [
+				roleId,
+				employeeId,
+			]);
+	}
+
+    // Find all roles I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+	findAllRoles() {
+		return this.connection
+			.promise()
+			.query(
+				'SELECT role_id id, title, department.name department, salary FROM roles LEFT JOIN department on roles.department_id = department.id'
+			);
+	}
+
+	// Create a new role
+	createRole(role) {
+		console.log(role);
+		return this.connection.promise().query('INSERT INTO roles SET ?', role);
+	}
+
+	// Remove a role from the db
+	removeRole(roleId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM role WHERE id = ?', roleId);
+	}
+
+	// Find all departments, join with employees and roles and sum up utilized department budget
+	findAllDepartments() {
+		return this.connection.promise().query('SELECT * FROM department');
+	}
+
+	// Create a new department
+	createDepartment(department) {
+		console.log(department);
+		return this.connection
+			.promise()
+			.query('INSERT INTO departments SET name = ?', department);
+	}
+
+	// Remove a department from the db
+	removeDepartment(departmentId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM department WHERE id = ?', departmentId);
+	}
+
+	// Find all employees in a given department, join with roles to display role titles
+	findAllEmployeesByDepartment(departmentId) {
+		return this.connection
+			.promise()
+			.query(
+				'SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id WHERE department.id = ?',
+				departmentId
+			);
+	}
+
+	// Find all employees by manager, join with departments and roles to display titles and department names
+	findAllEmployeesByManager(managerId) {
+		return this.connection
+			.promise()
+			.query(
+				'SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id WHERE manager_id = ?',
+				managerId
+			);
+	}
+
+	// create a new employee
+	createEmployee(employee) {
+		return this.connection
+			.promise()
+			.query('INSERT INTO employee SET ?', employee);
+	}
+
+	// update an employee's role
+	updateEmployeeRole(employeeId, roleId) {
+		return this.connection
+			.promise()
+			.query('UPDATE employee SET role_id = ? WHERE id = ?', [
+				roleId,
+				employeeId,
+			]);
+	}
+
+	// update an employee's manager
+	updateEmployeeManager(employeeId, managerId) {
+		return this.connection
+			.promise()
+			.query('UPDATE employees SET manager_id = ? WHERE employee_id = ?', [
+				managerId,
+				employeeId,
+			]);
+	}
+
+	// delete a department
+	deleteDepartment(departmentId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM department WHERE id = ?', departmentId);
+	}
+
+	// delete a role
+	deleteRole(roleId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM roles WHERE id = ?', roleId);
+	}
+
+	// delete an employee
+	deleteEmployee(employeeId) {
+		return this.connection
+			.promise()
+			.query('DELETE FROM employee WHERE id = ?', employeeId);
+	}
+
+	
+}
+
+module.exports = new DB(connection);
