f1a173e2d1c7f4234108e7c058283b336a5f0a7b
Scott Rinaberger <scottrinaberger@gmail.com>
2022-10-05 14:44:39 -0500

    added DB connection

---
+++ utils/connect.js
@@ -0,0 +1,14 @@
+// connect to the mysql database
+const mysql = require('mysql2');
+const startConnect = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	database: 'Notmypassword1',
+});
+
+startConnect.connect(function (err) {
+	if (err) throw err;
+	console.log('Connection started.');
+});
+
+module.exports = startConnect;
